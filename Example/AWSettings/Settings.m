5e5c92b713d9be65b9891110611e843843587ce7
Ashton Williams <AJWilliams@odecee.com.au>
2015-03-09 15:26:24 +1100

    Remove copyright header junk

--- Example/AWSettings/Settings.m
+++ Example/AWSettings/Settings.m
@@ -1,11 +1,3 @@
-//
-//  Settings.m
-//  AWSettings
-//
-//  Created by Ashton Williams on 7/03/2015.
-//  Copyright (c) 2015 Ashton Williams. All rights reserved.
-//
-
 #import "Settings.h"
 
 @implementation Settings
